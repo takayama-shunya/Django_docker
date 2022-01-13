83fafc38a96458b328bec85936135846082e56f0
ba520y <b.a520y@gmail.com>
2022-01-13 11:00:03 +0900

    fix STATIC_ROOT

--- Service_ID/root/settings.py
+++ Service_ID/root/settings.py
@@ -154,7 +154,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), PROJECT_DIR_NAME, 'static')
+
 
 IS_ON_LOG_FILE = False
 LOGGING = {
