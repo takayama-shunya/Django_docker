018647f524443071a74b3844aa18a57e03b6a42b
ba520y <b.a520y@gmail.com>
2022-01-13 12:24:15 +0900

    add urls for update_serv

--- Service_ID/peace_keeping/urls.py
+++ Service_ID/peace_keeping/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 from .views import CreateServiceStock, ValidateServiceStock, RegisterServiceStock
-from .views import GetNeeds, FuzzySearch, ShowServiceStock, ValidateUsedService, ConfirmUsedService, \
-    RegisterUsedService
+from .views import SelectUpdateServiceStock, ConfirmUpdateServiceStock, ResultUpdateServiceStock
+from .views import GetNeeds, FuzzySearch, ShowServiceStock, ValidateUsedService, ConfirmUsedService, RegisterUsedService
 
 app_name = 'peace_keeping'
 
@@ -11,6 +11,10 @@ urlpatterns = [
     path('error/<str:area_name>/', CreateServiceStock.as_view(), name='error'),
     path('validate_serv/', ValidateServiceStock.as_view(), name='validate_serv'),
     path('register_serv/', RegisterServiceStock.as_view(), name='register_serv'),
+    # -----
+    path('select_update_serv/', SelectUpdateServiceStock.as_view(), name='select_update_serv'),
+    path('confirm_update_serv/', ConfirmUpdateServiceStock.as_view(), name='confirm_update_serv'),
+    path('result_update_serv/', ResultUpdateServiceStock.as_view(), name='result_update_serv'),
     # -----
     path('get_needs/<str:area_name>/', GetNeeds.as_view(), name='get_needs'),
     path('fuzzy_search/<str:area_name>/<str:phrase>/', FuzzySearch.as_view(), name='fuzzy_search'),
