27f0796a9f8a6853fc452ed30ba52993b175495c
ba520y <b.a520y@gmail.com>
2022-01-15 12:48:38 +0900

    READMEs/setup_docker.md

--- docker/mysql/initdb.d/0010_backup.sql
+++ docker/mysql/initdb.d/0010_backup.sql
@@ -19,9 +19,6 @@
 -- Table structure for table `auth_group`
 --
 
-CREATE DATABASE IF NOT EXISTS `service_id`;
-USE `service_id`;
-
 DROP TABLE IF EXISTS `auth_group`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -860,4 +857,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-13 12:15:23
+-- Dump completed on 2022-01-14 14:58:49
